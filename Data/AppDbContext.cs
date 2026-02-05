b334c3c4ec5ff9721c888928fb0458628d6dc637
shahadAlroomi <shahad.ro@icloud.com>
2026-02-05 11:03:42 +0100

    add OnModelCreating

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,4 +7,17 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) {}
     public DbSet<Location> Locations {get;set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Location>().HasData(
+                new Location
+                {
+                    Id = 1,
+                    Name = "Main Office",
+                    Address = "Example Street 1",
+                    FloorsInfo = "PLAN 1\nCompany A\nCompany B",
+                }
+            );
+        }
 }
