51b41f2c3d4575f1ff6f2d5eb40676e49c957a0b
shahadAlroomi <shahad.ro@icloud.com>
2026-02-05 13:25:58 +0100

    add img + update index

---
+++ Migrations/20260205105953_AddExtraInfo.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infoscreen.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExtraInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExtraInfo",
+                table: "Locations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ExtraInfo", "FloorsInfo", "Name" },
+                values: new object[] { null, " Garage ", "Plan 1" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtraInfo",
+                table: "Locations");
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FloorsInfo", "Name" },
+                values: new object[] { "Garage", "PLAN 1" });
+        }
+    }
+}
