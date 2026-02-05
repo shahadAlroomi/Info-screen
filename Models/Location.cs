c18264587a98be596d95f805f288e655ca46a15a
shahadAlroomi <shahad.ro@icloud.com>
2026-02-05 10:24:55 +0100

    update class location

--- Models/Location.cs
+++ Models/Location.cs
@@ -2,7 +2,13 @@ namespace Infoscreen.Models;
 public class Location ()
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public string Address { get; set; }
+    public string? Address { get; set; }
+
+    public string? FloorsInfo {get;set;}
+
+    public string? LogoUrl {get;set;}
+
+    public string? ImageUrl {get;set;}
 }
